5549786eeb7979089b16d59bbb6e31851c374d01
satbir1992 <satbirsingh.singh4@gmail.com>
2023-07-28 13:24:09 +0800

    Category Table has been Created and Category Model Completed

---
+++ BathBombMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BathBombMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BathBombMVC.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
