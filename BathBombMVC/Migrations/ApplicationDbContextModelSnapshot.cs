fbd93dfc672afe80f0f104e618e4d703f1f20221
satbir1992 <satbirsingh.singh4@gmail.com>
2023-07-31 15:34:04 +0800

    Category Crud with views has been added and Razor Pages Project has been created

--- BathBombMVC/Migrations/ApplicationDbContextModelSnapshot.cs
+++ BathBombMVC/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -39,6 +39,26 @@ namespace BathBombMVC.Migrations
                     b.HasKey("Id");
 
                     b.ToTable("Categories");
+
+                    b.HasData(
+                        new
+                        {
+                            Id = 1,
+                            DisplayOrder = 1,
+                            Name = "Action"
+                        },
+                        new
+                        {
+                            Id = 2,
+                            DisplayOrder = 2,
+                            Name = "Scifi"
+                        },
+                        new
+                        {
+                            Id = 3,
+                            DisplayOrder = 3,
+                            Name = "History"
+                        });
                 });
 #pragma warning restore 612, 618
         }
